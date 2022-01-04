3021d97f4780f9830dc993adef841804e34ccea5
Warren Mazoue <wmaz@tauranga.govt.nz>
2022-01-04 20:03:04 +1300

    Basic ToDo List - in-memory storage

---
+++ BlazorToDo/Initialization/ServiceRegistration.cs
@@ -0,0 +1,13 @@
+﻿namespace BlazorToDo.Initialization
+{
+    public static class ServiceRegistration
+    {
+        public static IServiceCollection RegisterCoreServices(this IServiceCollection serviceCollection)
+        {
+            serviceCollection.AddRazorPages();
+            serviceCollection.AddServerSideBlazor();
+
+            return serviceCollection;
+        }
+    }
+}
